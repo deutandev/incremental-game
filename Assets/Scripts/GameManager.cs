1f9922270eb5948584a94a0727d81222e12a46a7
Andika Fathin Azfar <deutandev@pm.me>
2021-09-09 20:50:34 +0700

    Add scripts to change ResourceUI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// Add System.Serializable so the objects can be serialized and
+// its value can be set from inspector
+[System.Serializable]
+public struct ResourceConfig
+{
+    public string Name;
+    public double UnlockCost;
+    public double UpgradeCost;
+    public double Output;
+}
---
+++ Assets/Scripts/ResourceController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ResourceController : MonoBehaviour
+{
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+
+    private ResourceConfig _config;
+
+    private int _level = 1;
+
+    public void SetConfig (ResourceConfig config)
+    {
+        _config = config;
+
+        // ToString("0") used to remove zeros after comma
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+        ResourceUnlockCost.text = $"Unlock Cost\n{ _config.UnlockCost }";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
+    }
+
+    public double GetOutput()
+    {
+        return _config.Output*_level;
+    }
+
+    public double GetUpgradeCost()
+    {
+        return _config.UnlockCost;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
