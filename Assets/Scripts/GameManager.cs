129ee3c6cf6fcb854551257013266503356cfd03
Andika Fathin Azfar <deutandev@pm.me>
2021-09-10 13:12:50 +0700

    Show TapText in tap area and animate coin sprite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -37,9 +37,15 @@ public class GameManager : MonoBehaviour
     public Transform ResourcesParent; 
     public ResourceController ResourcePrefab; 
     public Text GoldInfo; 
-    public Text AutoCollectInfo; 
+    public Text AutoCollectInfo;
+
+    // Added in "Creating Incremental Tap"
+    public TapText TapTextPrefab;
+    public Transform CoinIcon;
 
     private List<ResourceController> _activeResources = new List<ResourceController> (); 
+    private List<TapText> _tapTextPool = new List<TapText> ();  // Added in "Creating Incremental Tap"
+    
     private float _collectSecond; 
     private double _totalGold;
 
@@ -58,7 +64,11 @@ public class GameManager : MonoBehaviour
         { 
             CollectPerSecond (); 
             _collectSecond = 0f; 
-        } 
+        }
+
+        // Added in "Creating Incremental Tap"
+        CoinIcon.transform.localScale = Vector3.LerpUnclamped (CoinIcon.transform.localScale, Vector3.one * 2f, 0.15f);
+        CoinIcon.transform.Rotate (0f, 0f, Time.deltaTime * -100f);
     }
 
     private void AddAllResources ()
@@ -94,5 +104,34 @@ public class GameManager : MonoBehaviour
     {
         _totalGold += value;
         GoldInfo.text = $"Gold: { _totalGold.ToString ("0") }";
+    }
+
+    // Added in "Creating Incremental Tap" 
+    public void CollectByTap (Vector3 tapPosition, Transform parent)
+    {
+        double output = 0;
+        foreach (ResourceController resource in _activeResources)
+        {
+            output += resource.GetOutput ();
+        }
+        TapText tapText = GetOrCreateTapText ();
+        tapText.transform.SetParent (parent, false);
+        tapText.transform.position = tapPosition;
+        tapText.Text.text = $"+{ output.ToString ("0") }";
+        tapText.gameObject.SetActive (true);
+        CoinIcon.transform.localScale = Vector3.one * 1.75f;
+        AddGold (output);
+    }
+    
+    // Added in "Creating Incremental Tap"
+    private TapText GetOrCreateTapText ()
+    {
+        TapText tapText = _tapTextPool.Find (t => !t.gameObject.activeSelf);
+        if (tapText == null)
+        {
+            tapText = Instantiate (TapTextPrefab).GetComponent<TapText> ();
+            _tapTextPool.Add (tapText);
+        }
+        return tapText;
     }
 }
